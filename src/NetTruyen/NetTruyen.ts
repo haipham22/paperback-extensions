267f412dcd222fd3ab2b4c911d467fa926f63dca
Hải Phạm <9427294+haipham22@users.noreply.github.com>
2024-01-11 12:31:42 +0700

    chore: change nettruyen url

--- src/NetTruyen/NetTruyen.ts
+++ src/NetTruyen/NetTruyen.ts
@@ -12,7 +12,7 @@ import { CheerioAPI } from 'cheerio'
 import { Request } from '@paperback/types/src/generated/Exports/Request'
 import { NetTruyenParser } from './NetTruyenParser'
 
-const siteUrl = 'https://www.nettruyenbing.com'
+const siteUrl = 'https://www.nettruyenclub.com'
 
 // noinspection JSUnusedGlobalSymbols
 export const NetTruyenInfo: SourceInfo = {
