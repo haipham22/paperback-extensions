1482ecb81e9a5f5371e84b3deb2041daf32e4d1b
haipham22 <9427294+haipham22@users.noreply.github.com>
2024-02-01 05:06:48 +0000

    Deploying to gh-pages from @ haipham22/paperback-extensions@d77433739f037181f4fdba485e46eb2aac4299a6 🚀

--- NetTruyen/source.js
+++ NetTruyen/source.js
@@ -6428,14 +6428,14 @@ exports.NetTruyen = exports.NetTruyenInfo = void 0;
 const types_1 = require("@paperback/types");
 const DefaultScrappy_1 = require("../DefaultScrappy");
 const NetTruyenParser_1 = require("./NetTruyenParser");
-const siteUrl = 'https://www.nettruyenclub.com';
+const siteUrl = 'https://www.nettruyenss.com';
 // noinspection JSUnusedGlobalSymbols
 exports.NetTruyenInfo = {
     name: 'NetTruyen',
     author: 'haipham22',
     contentRating: types_1.ContentRating.MATURE,
     icon: 'icon.png',
-    version: '2.0.6',
+    version: '2.0.7',
     description: 'NetTruyen Tracker',
     websiteBaseURL: siteUrl,
     intents: types_1.SourceIntents.MANGA_CHAPTERS |
