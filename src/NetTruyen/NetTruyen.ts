1a1782738f34f2f7f63ef31e3a16a375a5bdad77
Hải Phạm <9427294+haipham22@users.noreply.github.com>
2024-03-10 07:19:15 +0700

    Update NetTruyen.ts

--- src/NetTruyen/NetTruyen.ts
+++ src/NetTruyen/NetTruyen.ts
@@ -12,7 +12,7 @@ import { CheerioAPI } from 'cheerio'
 import { Request } from '@paperback/types/src/generated/Exports/Request'
 import { NetTruyenParser } from './NetTruyenParser'
 
-const siteUrl = 'https://www.nettruyenbb.com'
+const siteUrl = 'https://www.nettruyenee.com'
 
 // noinspection JSUnusedGlobalSymbols
 export const NetTruyenInfo: SourceInfo = {
@@ -20,7 +20,7 @@ export const NetTruyenInfo: SourceInfo = {
     author: 'haipham22',
     contentRating: ContentRating.MATURE,
     icon: 'icon.png',
-    version: '2.0.8',
+    version: '2.0.9',
     description: 'NetTruyen Tracker',
     websiteBaseURL: siteUrl,
     intents:
