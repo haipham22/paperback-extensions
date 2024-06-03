1949a93adde60879e57d7939f7f1d23349b60fef
haipham22 <9427294+haipham22@users.noreply.github.com>
2024-06-03 10:52:26 +0000

    Deploying to gh-pages from @ haipham22/paperback-extensions@0c969782b49b3ab21a64317a27ea1403bf8efa2f 🚀

--- NetTruyen/index.js
+++ NetTruyen/index.js
@@ -6428,7 +6428,7 @@ exports.NetTruyen = exports.NetTruyenInfo = void 0;
 const types_1 = require("@paperback/types");
 const DefaultScrappy_1 = require("../DefaultScrappy");
 const NetTruyenParser_1 = require("./NetTruyenParser");
-const siteUrl = 'https://www.nettruyencc.com';
+const siteUrl = 'https://nettruyencc.com';
 // noinspection JSUnusedGlobalSymbols
 exports.NetTruyenInfo = {
     name: 'NetTruyen',
